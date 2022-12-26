b74d99de079d37cc3e800d5be9f3bf868c689c13
Minwoo <hongminooo@hanmail.net>
2022-12-26 18:55:19 +0900

    Add to SonySpeaker

---
+++ 05.spring/lec01_basic/src/main/java/com/lec/basic/poly3/SonySpeaker.java
@@ -0,0 +1,8 @@
+package com.lec.basic.poly3;
+
+public class SonySpeaker implements Speaker {
+
+	public SonySpeaker() {System.out.println("==> Sony Speaker 객체 생성"); }
+	public void volumnUp() { System.out.println("==> Sonny Speaker 볼륨업"); }
+	public void volumnDown() { System.out.println("==> Sonny Speaker 볼륨다운"); }
+}
