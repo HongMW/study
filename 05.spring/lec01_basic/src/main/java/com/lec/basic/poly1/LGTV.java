8f1215e412cfa9e8ee6283bf90d71378d5ce28d6
Minwoo <hongminooo@hanmail.net>
2022-12-26 18:52:12 +0900

    Add to LGTV

---
+++ 05.spring/lec01_basic/src/main/java/com/lec/basic/poly1/LGTV.java
@@ -0,0 +1,10 @@
+package com.lec.basic.poly1;
+
+public class LGTV implements TV{
+
+	public LGTV() { System.out.println("==> LGTV() 생성자호출!!"); }
+	public void powerOn() { System.out.println("LG TV - pwerOn!!");}
+	public void powerOff() { System.out.println("LG TV - pwerOff!!");}
+	public void volumnUp() { System.out.println("LG TV - volumnup!!");}
+	public void volumnDown() { System.out.println("LG TV - volumndown!!");}
+}
