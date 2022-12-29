15d12fd355129e191caa6ab87b6a7b8d03582f70
Minwoo <hongminooo@hanmail.net>
2022-12-29 22:37:41 +0900

    Add to BoardService java

---
+++ 05.spring/lec04_jdbc/src/main/java/com/lec/jdbc/service/BoardService.java
@@ -0,0 +1,17 @@
+package com.lec.jdbc.service;
+
+import java.util.List;
+
+import com.lec.jdbc.vo.BoardVO;
+import com.lec.jdbc.vo.PageInfo;
+
+public interface BoardService {
+
+	void insertBoard(BoardVO board);
+	int deleteBoard(int seq);
+	void updateBoard(BoardVO board);
+	BoardVO selectBoard(int seq);
+	List<BoardVO> getBoardList(int p, int perPage);
+	PageInfo getPageInfo(int p, int perPage);
+
+}
