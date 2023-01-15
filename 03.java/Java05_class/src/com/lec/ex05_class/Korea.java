df4f7ff357a12b89e22de2098b5f8bc87ec95ead
Minwoo <hongminooo@hanmail.net>
2023-01-15 22:07:16 +0900

    Add to Korea java

---
+++ 03.java/Java05_class/src/com/lec/ex05_class/Korea.java
@@ -0,0 +1,12 @@
+package com.lec.ex05_class;
+
+public class Korea extends Contry {
+
+	@Override
+	void speak() {
+	System.out.println("한국어로 말을 합니다.");
+}
+	void move() {
+		System.out.println("프로그램을 개발 합니다.");
+	}
+}
