60486bb6a0f744fd2977019692baa95071fafa80
Minwoo <hongminooo@hanmail.net>
2023-01-15 22:05:50 +0900

    Add to Contry java

---
+++ 03.java/Java05_class/src/com/lec/ex05_class/Contry.java
@@ -0,0 +1,17 @@
+package com.lec.ex05_class;
+
+public class Contry {
+	
+	String name;
+	String gender;
+	String lang;
+	int age;
+	
+	void speak() {
+		System.out.println("말을 합니다");
+	}
+	void move() {
+		System.out.println("움직인다!!");
+	}
+
+}
