e629455ee1933e36ac242065264941bcb975af64
Minwoo <hongminooo@hanmail.net>
2023-01-16 18:07:05 +0900

    Add to C java

---
+++ 03.java/Java05_class/src/com/lec/ex17_access/consructor/pkg2/C.java
@@ -0,0 +1,11 @@
+package com.lec.ex17_access.consructor.pkg2;
+
+import com.lec.ex17_access.consructor.pkg1.A;
+
+public class C {
+
+	A a1 = new A(false);	// public 
+//	A a2 = new A(1.0);		// protected (x)
+//	A a3 = new A(1);		// default (x)
+//	A a4 = new A("A");		// private (x)
+}
