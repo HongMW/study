1726a2c2259b84c527197ccb249c34170fa3d862
Minwoo <hongminooo@hanmail.net>
2023-01-16 18:00:28 +0900

    Add to Car java

---
+++ 03.java/Java05_class/src/com/lec/ex10_constructor/Main/Car.java
@@ -0,0 +1,10 @@
+package com.lec.ex10_constructor.Main;
+
+public class Car {
+
+	// 생성자
+	Car() {}
+	Car(String color, int cc) {}
+	Car(int cc, String color) {}
+	Car(String color, int cc, String model) {}
+}
