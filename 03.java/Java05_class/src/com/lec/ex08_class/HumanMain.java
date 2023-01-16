4f14f99a463833b027d90aaeae07259ba9aab58b
Minwoo <hongminooo@hanmail.net>
2023-01-16 17:59:01 +0900

    Add to HumanMain java

---
+++ 03.java/Java05_class/src/com/lec/ex08_class/HumanMain.java
@@ -0,0 +1,22 @@
+package com.lec.ex08_class;
+
+public class HumanMain {
+
+	public static void main(String[] args) {
+		
+		//인터페이스(interface)
+		Korean kor = new Korean();
+		kor.speak("한글");
+		//kor.nation = "대한민국"; // 에러
+		kor.move();
+		kor.think();
+		System.out.println();
+		
+		
+		American usa = new American();
+		usa.speak("영어");
+		usa.move(); 
+		usa.think();
+	}
+
+}
