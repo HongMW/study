5bc549c2d5cfecc0c80568074c4f7d247455917c
Minwoo <hongminooo@hanmail.net>
2023-01-17 18:15:32 +0900

    Add to Parent java

---
+++ 03.java/java06.inheritance/src/com/lec/ex08_casting/Parent.java
@@ -0,0 +1,8 @@
+package com.lec.ex08_casting;
+
+public class Parent {
+
+	public String field1;
+	public void method1() {System.out.println("Parnet.method1 호출!!!");}
+	public void method2() {System.out.println("Parnet.method2 호출!!!");}
+}
