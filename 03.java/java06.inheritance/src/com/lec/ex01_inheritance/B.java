0c44a1f08436df79a029170f0bb8466c4ba1687c
Minwoo <hongminooo@hanmail.net>
2023-01-19 18:34:35 +0900

    Add to B java

---
+++ 03.java/java06.inheritance/src/com/lec/ex01_inheritance/B.java
@@ -0,0 +1,17 @@
+package com.lec.ex01_inheritance;
+
+public class B extends A {
+
+	@Override
+	public void method1() {
+		// TODO Auto-generated method stub
+		super.method1();
+	}
+
+	@Override
+	public void method2() {
+		// TODO Auto-generated method stub
+		super.method1();
+	}
+
+}
