d49488906f7698b4203b2441ae8b2a3edc917e69
Minwoo <hongminooo@hanmail.net>
2023-01-20 18:02:49 +0900

    Add to American java

---
+++ 03.java/java06.inheritance/src/com/lec/ex01_inheritance/American.java
@@ -0,0 +1,21 @@
+package com.lec.ex01_inheritance;
+
+public class American extends Eve{
+
+	@Override
+	public String toString() {
+		return "나는 미국사람입니다! 니의 이름은 = " + this.name + "입니다";
+	}
+	
+	@Override
+	public void move() {
+		System.out.println("화장품을 제조합니다!");
+	}
+	
+	@Override
+	public void speak() {
+		System.out.println("영어로 말을 합니다!");
+	}
+	
+	
+}
