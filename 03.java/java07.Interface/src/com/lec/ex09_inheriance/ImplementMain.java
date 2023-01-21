2f22290b3a252e8323bb77923292f3906ae408de
Minwoo <hongminooo@hanmail.net>
2023-01-21 22:07:41 +0900

    Add to ImplementMain java

---
+++ 03.java/java07.Interface/src/com/lec/ex09_inheriance/ImplementMain.java
@@ -0,0 +1,35 @@
+package com.lec.ex09_inheriance;
+
+public class ImplementMain {
+
+	public static void main(String[] args) {
+		
+		ImplementC im = new ImplementC();
+		im.methodA();
+		im.methodB();
+		im.methodC();
+		System.out.println();
+		
+		// 1. im -> InterfaceA
+		InterfaceA ia = im;
+		ia.methodA();
+//		ia.methodB();
+//		ia.methodC();
+		System.out.println();
+		
+		
+		// 2. im -> InterfaceB
+		InterfaceB ib = im;
+//		ib.methodA();
+		ib.methodB();
+//		ib.methodC();
+		System.out.println();
+		
+		// 3. im -> InterfaceC
+		InterfaceC ic = im;
+		ic.methodA();
+		ic.methodB();
+		ic.methodC();
+	}
+
+}
