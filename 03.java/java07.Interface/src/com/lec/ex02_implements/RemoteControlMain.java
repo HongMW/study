97d897aa4f26c6f27904c2583040625aed14ba36
Minwoo <hongminooo@hanmail.net>
2023-01-24 17:19:09 +0900

    Add to RemoteControlMain java

---
+++ 03.java/java07.Interface/src/com/lec/ex02_implements/RemoteControlMain.java
@@ -0,0 +1,25 @@
+package com.lec.ex02_implements;
+
+public class RemoteControlMain {
+
+	public static void main(String[] args) {
+		
+		// Audio audio = new Audio();
+		// Television tv = new Television();
+		
+		RemoteControl rc;
+		rc = new Television();
+		rc.powerOn();
+		rc.setVolume(5);
+		rc.powerOff();
+		System.out.println();
+		
+		
+		rc = new Audio();
+		rc.powerOn();
+		rc.setVolume(5);
+		rc.powerOff();
+		System.out.println();
+	}
+
+}
