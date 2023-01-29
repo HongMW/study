0023242fae0ec73d9f2382d45762303e93692d02
Minwoo <hongminooo@hanmail.net>
2023-01-29 23:20:54 +0900

    Add to SwordUltraJJang java

---
+++ 03.java/java10.summarize/src/com/robot/impl/SwordUltraJJang.java
@@ -0,0 +1,12 @@
+package com.robot.impl;
+
+import com.robot.inter.InterSword;
+
+public class SwordUltraJJang implements InterSword{
+
+	@Override
+	public void sword() {
+		System.out.println("울트라짱 검이 있습니다!!");
+	}
+
+}
